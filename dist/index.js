9ab4c1e2d24908032637dee0fa0930428b5608f3
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-02-21 09:31:08 -0300

    Provide Babel support

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var _yargs = require('yargs');
+
+var _yargs2 = _interopRequireDefault(_yargs);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log(1);
+//# sourceMappingURL=index.js.map
