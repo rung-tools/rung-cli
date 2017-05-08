bdd56155ad03355eb7f0998d0a132ee1a8aac7b4
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-05-08 17:55:05 -0300

    Small bug

--- src/readme.js
+++ src/readme.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import { all } from 'bluebird';
+import { all, promisify } from 'bluebird';
 import { compile } from 'handlebars';
 import {
     keys,
