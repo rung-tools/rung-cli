58caaf14c89e09b28a634bebc3778f3ce20cef05
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-05-08 11:10:07 -0300

    Start reading package!

--- src/readme.js
+++ src/readme.js
@@ -1,5 +1,9 @@
+import fs from 'fs';
 import { resolve } from 'bluebird';
+import { getProperties } from './vm';
+import { readFile } from './run';
 
 export default function readme() {
-    return resolve(1);
+    return readFile('package.json', 'utf-8')
+        .then(JSON.parse);
 }
