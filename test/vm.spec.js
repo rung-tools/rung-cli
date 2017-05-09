986141bd369b43f4a5c2f333918d4fab0b14f8e6
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-05-09 14:19:23 -0300

    Add test coverage reports

--- test/vm.spec.js
+++ test/vm.spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { __require } from '../src/vm';
+import { __require, getProperties, runAndGetAlerts } from '../src/vm';
 
 describe('vm.js', () => {
     describe('Module system', () => {
@@ -12,6 +12,46 @@ describe('vm.js', () => {
             expect(() => {
                 const lib = __require(['rung-sdk', 'moment'], 'fs');
             }).to.throw(/Disallowed dependency/);
+        });
+    });
+
+    describe('Virtual machine runtime', () => {
+        it('should get the config of an extension', () => {
+            const source = 'module.exports = { extension: () => {}, config: { primaryKey: true } };';
+
+            return getProperties({ name: 'test-config', source })
+                .then(result => {
+                    expect(result).property('primaryKey').to.be.true;
+                });
+        });
+
+        it('should get the alerts of a synchronous extension', () => {
+            const source = 'module.exports = { extension: ctx => ([`Hello, ${ctx.name}!`]) };'
+
+            return runAndGetAlerts({ name: 'test-alerts', source }, { name: 'Marcelo' })
+                .then(alerts => {
+                    expect(alerts).to.be.an('array');
+                    expect(alerts).property(0).to.be.equals('Hello, Marcelo!');
+                });
+        });
+
+        it('should get the alerts of a asynchronous extension', () => {
+            const source = `
+                const { delay } = require('bluebird');
+
+                module.exports = {
+                    extension: (ctx, done) => {
+                        delay(200).then(() => {
+                            done(['It works!']);
+                        });
+                    }
+                };
+            `;
+
+            return runAndGetAlerts({ name: 'test-alerts', source }, { name: 'Marcelo' })
+                .then(alerts => {
+                    expect(alerts).to.be.an('array');
+                });
         });
     })
 });
