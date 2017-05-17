8faa280f706b5c3756e31ca7b97302e4c0d53302
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-05-17 16:14:21 -0300

    Destructuring of tests

--- test/db.spec.js
+++ test/db.spec.js
@@ -14,36 +14,41 @@ const home = os.homedir();
 const extensionName = 'rung-database-test';
 const dbPath = path.join(home, '.rung', `${extensionName}.db`);
 
-function sourceWithDb(db) {
-    return compileES6(`
-        export default {
-            extension(context) {
-                return {
-                    alerts: {},
-                    db: ${JSON.stringify(db)}
-                };
-            }
-        };
-    `);
-}
-
 describe.only('db.js', () => {
     before(() => rm(dbPath));
 
     describe('Database', () => {
         it('should store an object in the database', () => {
-            const source = sourceWithDb({
-                counter: 1
-            });
+            const source = compileES6(`
+                export default {
+                    extension(context) {
+                        return {
+                            alerts: {},
+                            db: { counter: 1 }
+                        };
+                    }
+                };
+            `);
 
             return runAndGetAlerts({ name: extensionName, source }, {})
                 .then(result => {
+                    expect(result.db.counter).to.equals(1);
                     expect(dbPath).to.be.a.file();
                 });
         });
 
+        it('should read and update data from db');
+
         it('should drop the file when passed undefined', () => {
-            const source = sourceWithDb(undefined);
+            const source = compileES6(`
+                export default {
+                    extension(context) {
+                        return {
+                            alerts: {}
+                        };
+                    }
+                };
+            `);
 
             return runAndGetAlerts({ name: extensionName, source }, {})
                 .then(() => {
