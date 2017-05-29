d68f3bc3b4599699847c281f881349edef4f80a5
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-05-29 10:41:40 -0300

    Use spinner to show progress

--- src/publish.js
+++ src/publish.js
@@ -2,8 +2,10 @@ import fs from 'fs';
 import { all, resolve, promisify } from 'bluebird';
 import { curry, isNil, pick, pipe } from 'ramda';
 import { gray } from 'colors';
+import { Spinner } from 'cli-spinner';
 import superagent from 'superagent';
 import { isURL } from 'validator';
+import { green } from 'colors/safe';
 import { emitWarning, IO } from './input';
 import build from './build';
 
@@ -34,15 +36,17 @@ function fetchRungApi(io) {
  * After login, publishes the extension
  *
  * @param {String} api
+ * @param {Spinner} spinner
  * @param {String} path
  * @return {Promise}
  */
-const publishPackage = curry((api, path) => {
+const publishPackage = curry((api, spinner, path) => {
     const io = IO();
 
     return resolve(request.get(`${api}/whoami`))
         .then(({ body }) => io.print(`Logged in as ${body.exhibitionName || body.name}`))
         .then(() => readFile('package.json', 'utf-8').then(pipe(JSON.parse, pick(['name', 'version']))))
+        .tap(() => spinner.stop(true))
         .then(({ name, version }) => request.post(`${api}/metaExtensions`)
             .attach('metaExtension', path)
             .then(() => io.print(`Successfully published ${name}@${version}`)))
@@ -58,11 +62,15 @@ const publishPackage = curry((api, path) => {
 export default function publish(args) {
     const io = IO();
     const api = fetchRungApi(io);
+    const spinner = new Spinner(green('%s publishing extension...'));
+    spinner.setSpinnerString(8);
 
     return io.read(gray('Rung email'))
         .then(email => all([email, io.password(gray('Rung password'))]))
+        .tap(() => spinner.start())
         .spread((email, password) => request.post(`${api}/login`)
             .send({ email, password }))
         .then(() => build(args))
-        .then(publishPackage(api));
+        .then(publishPackage(api, spinner))
+        .tapCatch(() => spinner.stop(true));
 }
