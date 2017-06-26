9c02377f259a3b0bb24ed326b24b6f37d8c79910
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-06-26 10:54:54 -0300

    Forbid try catch

--- src/vm.js
+++ src/vm.js
@@ -2,6 +2,7 @@ import { NodeVM } from 'vm2';
 import Promise, { reject, resolve } from 'bluebird';
 import {
     propOr,
+    tryCatch,
     type
 } from 'ramda';
 import { compileHTML } from './compiler';
@@ -29,12 +30,12 @@ function runInSandbox(name, source, strings = {}) {
     vm.freeze(compileHTML, '__render__');
     vm.freeze(translator(strings), '_');
 
-    try {
+    const evaluate = tryCatch(() => {
         const result = vm.run(source, `${name}.js`);
         return resolve(propOr(result, 'default', result));
-    } catch (err) {
-        return reject(err);
-    }
+    }, reject);
+
+    return evaluate();
 }
 
 /**
