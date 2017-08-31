d6ea25999a3a58127a08277dda7ec5ee99ed03b1
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-08-31 10:26:48 -0300

    :top: WOW SUCH & OPERATOR!!!

--- src/types.js
+++ src/types.js
@@ -64,7 +64,7 @@ export const filter = {
     Char: take,
     Double: parseFloat,
     Integer: parseInt(_, 10),
-    Money: tryCatch(pipe(replace(',', '.'), parseFloat), always(NaN))
+    Money: tryCatch(replace(',', '.') & parseFloat, always(NaN))
 };
 
 // Type validators
