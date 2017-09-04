806e46446302af2e9cb01dec5afa54a988696700
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-09-04 08:23:40 -0300

    Remove broken test

--- test/boilerplate.spec.js
+++ test/boilerplate.spec.js
@@ -12,54 +12,7 @@ chai.use(json);
 
 const rm = promisify(rimraf);
 
-describe.only('boilerplate.js', () => {
+describe('boilerplate.js', () => {
     describe('Input and output', () => {
-        it('should create a boilerplate', () => {
-            return all([boilerplate(), delay(500)
-                .then(() => stdin.write())]);
-        }).timeout(15000);
     });
 });
-
-/*
-
-        it('should answer the questions and create a valid extension', () => {
-            const stream = createStream(['boilerplate']);
-            const next = text => () => stream.write(`${text}\r`)
-                .then(() => stream.once('data'));
-            const stop = (text = '') => () => stream.write(`${text}\r`);
-
-            const answerAll = () => stream.once('data')
-                .then(next('boilerplate-project'))
-                .then(next('0.1.0'))
-                .then(next('Boilerplate title'))
-                .then(next('Boilerplate description'))
-                .then(next('miscellaneous'))
-                .then(stop('MIT'))
-                .then(() => delay(1000));
-
-            return answerAll()
-                .then(() => {
-                    const dir = path.join(__dirname, '..', 'boilerplate-project');
-                    expect(dir).to.be.a.directory().with.files([
-                        'README.md',
-                        'package.json',
-                        'index.js'
-                    ]);
-                    expect(path.join(dir, 'package.json')).to.be.a.file()
-                        .with.json.using.schema({
-                            title: 'package.json schema',
-                            type: 'object',
-                            required: ['name', 'version', 'category', 'license'],
-                            properties: {
-                                name: { type: 'string' },
-                                version: { type: 'string' }
-                            }
-                        });
-                })
-                .tap(() => rm('boilerplate-project'))
-                .finally(stream.close);
-        }).timeout(20000);
-    });
-});
-*/
