fd092378fa447f9ef0c625fde6cacf150fad1eff
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-09-04 17:55:08 -0300

    THE STATE OF THE ART TESTER

--- test/boilerplate.spec.js
+++ test/boilerplate.spec.js
@@ -1,7 +1,9 @@
 import path from 'path';
-import chai from 'chai';
+import { resolve } from 'bluebird';
+import chai, { expect } from 'chai';
 import fs from 'chai-fs';
 import json from 'chai-json-schema';
+import { createStream, monkey, ENTER } from './helper';
 
 chai.use(fs);
 chai.use(json);
@@ -9,7 +11,19 @@ chai.use(json);
 describe('boilerplate.js', () => {
     describe('Input and output', () => {
         it.only('should correctly deal with the questions', () => {
+            const stream = createStream(['boilerplate']);
 
-        });
+            return monkey(stream, [
+                'somewhere',
+                ENTER,
+                '0.2.0',
+                ENTER,
+                'fooo',
+                ENTER
+            ])
+                .then(out => {
+                    console.log('8888', out, '9999');
+                })
+        }).timeout(15000);
     });
 });
--- test/helper.js
+++ test/helper.js
@@ -1,11 +1,12 @@
 import { spawn } from 'child_process';
-import { promisify } from 'bluebird';
+import concat from 'concat-stream';
+import Promise, { delay, promisify } from 'bluebird';
 
 const promisifyStream = fn => promisify((param, callback) => {
     fn(param, callback(null, _));
 });
 
-export function createCustomStream(env, args, cmd = 'dist/cli.js') {
+export function createCustomStream(env, args = [], cmd = 'dist/cli.js') {
     const task = spawn('node', [cmd, ...args], { stdio: 'pipe', env });
     task.stdout.setEncoding('utf-8');
 
@@ -14,10 +15,33 @@ export function createCustomStream(env, args, cmd = 'dist/cli.js') {
         on: promisifyStream(task.stdout.on.bind(task.stdout)),
         write: promisifyStream(task.stdin.write.bind(task.stdin)),
         after: promisifyStream(task.stdin.on.bind(task.stdin, 'close')),
-        close: ~task.kill('SIGTERM')
+        close: ~task.kill('SIGTERM'),
+        process: ~task
     };
 }
 
-export function createStream(args, cmd = 'dist/cli.js') {
+export function createStream(args = [], cmd = 'dist/cli.js') {
     return createCustomStream({}, args, cmd);
+}
+
+export const ENTER = '\x0D';
+export const DOWN = '\x1B\x5B\x42';
+export const UP = '\x1B\x5B\x41';
+
+export function monkey(stream, combo = [], timeout = 500) {
+    const task = stream.process();
+
+    const eventLoop = combo => {
+        if (combo.length > 0) {
+            const [head, ...tail] = combo;
+            return delay(timeout)
+                .then(~stream.write(head))
+                .then(~eventLoop(tail));
+        }
+
+        task.stdin.end();
+    };
+
+    eventLoop(combo);
+    return new Promise(concat & task.stdout.pipe);
 }
