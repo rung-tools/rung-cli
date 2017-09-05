17d36d68c7f15de7e5260c5bc7b7416a0406b4f1
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-09-05 11:56:53 -0300

    First functional test with our monkeys

--- test/boilerplate.spec.js
+++ test/boilerplate.spec.js
@@ -1,29 +1,69 @@
-import path from 'path';
-import { resolve } from 'bluebird';
+import { promisify } from 'bluebird';
 import chai, { expect } from 'chai';
+import { join } from 'ramda';
 import fs from 'chai-fs';
 import json from 'chai-json-schema';
-import { createStream, monkey, ENTER } from './helper';
+import rimraf from 'rimraf';
+import {
+    clearAnsiEscapes,
+    createStream,
+    press,
+    spawnMonkey
+} from './helper';
 
 chai.use(fs);
 chai.use(json);
 
+const rm = promisify(rimraf);
+
 describe('boilerplate.js', () => {
     describe('Input and output', () => {
+        after(~rm('very-cool-project'));
+        before(~rm('very-cool-project'));
+
         it.only('should correctly deal with the questions', () => {
             const stream = createStream(['boilerplate']);
+            const combo = [
+                'very-cool-project', press.ENTER,
+                '0.2.1', press.ENTER,
+                'Very Cool Project', press.ENTER,
+                'This is only a test', press.ENTER,
+                press.UP, press.ENTER, // Navigate in categories list
+                press.ENTER
+            ];
 
-            return monkey(stream, [
-                'somewhere',
-                ENTER,
-                '0.2.0',
-                ENTER,
-                'fooo',
-                ENTER
-            ])
-                .then(out => {
-                    console.log('8888', out, '9999');
-                })
-        }).timeout(15000);
+            return spawnMonkey(stream, { combo, procrastination: 1000 })
+                .then(clearAnsiEscapes)
+                .tap(output => {
+                    const expected = [
+                        '? Project name (rung-cli) ',
+                        '? Version (1.0.0) ',
+                        '? Title (Untitled) ',
+                        '? Description ',
+                        '? Category (Use arrow keys)',
+                        '  Asset Maintenance ',
+                        '  Fleet Management ',
+                        '  Games ',
+                        '❯ Miscellaneous ',
+                        '  Movies & TV ',
+                        '  Occupational Health and Safety ',
+                        '  Retail ',
+                        '(Move up and down to reveal more choices)',
+                        '  Travel ',
+                        '  Asset Maintenance ',
+                        '  Fleet Management ',
+                        '❯ Games ',
+                        '  Miscellaneous ',
+                        '  Movies & TV ',
+                        '  Occupational Health and Safety ',
+                        '(Move up and down to reveal more choices)',
+                        '? license (MIT) ',
+                        ' ✔ Success: project generated',
+                        ''
+                    ] | join('\n');
+
+                    expect(output).to.equals(expected);
+                });
+        }).timeout(30000);
     });
 });
--- test/helper.js
+++ test/helper.js
@@ -1,6 +1,14 @@
 import { spawn } from 'child_process';
 import concat from 'concat-stream';
 import Promise, { delay, promisify } from 'bluebird';
+import {
+    complement,
+    equals,
+    join,
+    map,
+    split,
+    takeWhile
+ } from 'ramda';
 
 const promisifyStream = fn => promisify((param, callback) => {
     fn(param, callback(null, _));
@@ -24,19 +32,42 @@ export function createStream(args = [], cmd = 'dist/cli.js') {
     return createCustomStream({}, args, cmd);
 }
 
-export const ENTER = '\x0D';
-export const DOWN = '\x1B\x5B\x42';
-export const UP = '\x1B\x5B\x41';
+/**
+ * Returns the visible characters from a byte array (represented as string)
+ *
+ * @param {String} byteArray
+ * @return {String}
+ */
+export const clearAnsiEscapes = split('\n')
+    & map(split('') & takeWhile(complement(equals('\u001b'))) & join(''))
+    & join('\n');
 
-export function monkey(stream, combo = [], timeout = 500) {
+/**
+ * Ansi escape codes for keypress
+ */
+export const press = {
+    ENTER: '\x0D',
+    DOWN: '\x1B\x5B\x42',
+    UP: '\x1B\x5B\x41'
+};
+
+/**
+ * Spawns a monkey to work.
+ * Creates an IO event loop to work on dynamic buffered input and output.
+ * Receives a stream, a combo and a timeout, where the combo may contain
+ * ansi escape codes to common CLI operations.
+ *
+ * @param {Object} stream - the promisified stream object
+ * @param {Object} opts - The options to the monkey
+ */
+export function spawnMonkey(stream, { combo = [], procrastination = 500 }) {
     const task = stream.process();
 
-    const eventLoop = combo => {
-        if (combo.length > 0) {
-            const [head, ...tail] = combo;
-            return delay(timeout)
-                .then(~stream.write(head))
-                .then(~eventLoop(tail));
+    const eventLoop = ([head, ...tail]) => {
+        if (head) {
+            return delay(procrastination)
+                .tap(~stream.write(head))
+                .tap(~eventLoop(tail));
         }
 
         task.stdin.end();
