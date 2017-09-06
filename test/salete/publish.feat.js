ffb34e6d05a76f05ed02ae1bfc3042fa8875a808
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-09-06 19:14:43 -0300

    Cover different cases

--- test/salete/publish.feat.js
+++ test/salete/publish.feat.js
@@ -3,7 +3,7 @@ import { expect } from 'chai';
 import work, { keepCalm, keyboard } from './salete';
 
 const { press, type } = keyboard;
-const publish = (args = []) => ({
+const publish = (args = [], env = {}) => ({
     runs: ['node', '../dist/cli.js', 'publish', ...args],
     does: [
         type('salete'), press.ENTER,
@@ -11,9 +11,7 @@ const publish = (args = []) => ({
         type('ilovecapybaras'), press.ENTER,
         keepCalm(10)
     ],
-    env: {
-        RUNG_API: 'http://35.165.157.18/api/'
-    }
+    env
 });
 
 export default () => {
@@ -29,11 +27,24 @@ export default () => {
     }).timeout(keepCalm(60));
 
     it('should at least try to reference a *.rung binary to upload to another api', () => {
-        return work(publish(['--file=salete-hello-world.rung', '--private']))
+        return work(
+            publish(['--file=salete-hello-world.rung', '--private']), {
+                RUNG_API: 'http://35.165.157.18/api/'
+            })
             .then(output => {
                 expect(output).to.contain('Unauthorized');
             });
     }).timeout(keepCalm(60));
+
+    it('should emit warning for a wrong URL', () => {
+        return work(
+            publish(['--file=salete-hello-world.rung', '--private']), {
+                RUNG_API: 'trololololololo'
+            })
+            .then(output => {
+                expect(output).to.contain('invalid API for Rung');
+            });
+    })
 
     after(~process.chdir('..'));
 };
