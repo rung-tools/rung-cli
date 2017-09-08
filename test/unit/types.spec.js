4e371dbc3f22d19dbe450c318b6e03059d6cd317
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-09-08 14:54:30 -0300

    FORGOT ONE SINGLE CASE

--- test/unit/types.spec.js
+++ test/unit/types.spec.js
@@ -9,13 +9,15 @@ export default () => {
             t.IntegerRange(10, 20),
             t.DoubleRange(10, 20),
             t.OneOf(['Scala', 'Haskell', 'Elixir']),
-            t.IntegerMultiRange(10, 20)
+            t.IntegerMultiRange(10, 20),
+            t.Color
         ] | map(t.getTypeName)).to.deep.equals([
             'Char(8)',
             'IntegerRange(10, 20)',
             'DoubleRange(10, 20)',
             'OneOf([Scala, Haskell, Elixir])',
-            'IntegerMultiRange(10, 20)'
+            'IntegerMultiRange(10, 20)',
+            'Color'
         ]);
     });
 
