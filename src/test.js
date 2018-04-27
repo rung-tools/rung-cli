760bbd73f9770a4eab50817830ee0cd348c7eaaa
Marcelo Camargo <marcelocamargo@linuxmail.org>
2018-04-27 17:04:53 -0300

    Y U NO PASS

--- src/test.js
+++ src/test.js
@@ -7,6 +7,6 @@ export const readFile = promisify(fs.readFile);
 export default async () =>
     readFile('test/index.js', 'utf-8')
         .then(content => {
-            console.log(content);
+            console.log('OK, GOOD BOY');
         })
         .catch(~emitError('No tests to run. [test/index.js] not found'));
