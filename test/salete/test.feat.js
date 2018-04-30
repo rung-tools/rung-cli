9f4c4d537501fb7c51b2f6b445e1a8a282721816
Marcelo Camargo <marcelocamargo@linuxmail.org>
2018-04-30 09:58:58 -0300

    Ok, now the test is passing!

--- test/salete/test.feat.js
+++ test/salete/test.feat.js
@@ -1,9 +1,12 @@
+import process from 'process';
 import { expect } from 'chai';
 import work, { keepCalm } from './salete';
 
 // Inception! Mocha runs Salete that runs RungTests written by the user that are
 // written on top of Mocha and Chai. Too proud of it!
 export default () => {
+    before(~process.chdir('salete-hello-world'));
+
     it('should emit an error for Salete when she runs `rung test` without tests', () => {
         return work({
             runs: ['node', '../dist/cli.js', 'test'],
@@ -12,4 +15,6 @@ export default () => {
                 expect(output).to.contain('Error: No tests to run. [test/index.js] not found');
             });
     }).timeout(keepCalm(40));
+
+    after(~process.chdir('..'));
 };
