c3bbb9ffc4df8d44d886c2e4c84aa12d4aa1c7b4
Marcelo Camargo <marcelocamargo@linuxmail.org>
2018-05-02 11:09:18 -0300

    Compile app to V8 object and test to ES6, create VM instance

--- src/test.js
+++ src/test.js
@@ -1,39 +1,50 @@
 import fs from 'fs';
-import { promisify, reject } from 'bluebird';
+import { all, promisify, reject } from 'bluebird';
+import { compileES6 } from './compiler';
 import { read } from './db';
 import { getLocale, getLocaleStrings } from './i18n';
+import { compileModulesFromSource, evaluateModules } from './module';
 import { compileSources } from './run';
 import { createVM, runInSandbox } from './vm';
 
 export const readFile = promisify(fs.readFile);
 
 /**
- * Creates a VM for testing purposes, with access to an `app` object containing
- * `extension` closure and `config` object.
+ * Compiles the test file, links required modules and returns an instance of
+ * a virtual machine to run them.
  *
- * @param {Object} app - containing `extension` and `config`
- * @return {NodeVM}
+ * @return {Promise}
  */
-function createTestVM(app) {
-    // TODO: Compile test file to ES5
-    // TODO: Allow to require inner modules
+async function compileTests() {
+    const source = await readFile('test/index.js', 'utf-8')
+        | compileES6;
+    const modules = await compileModulesFromSource(source);
     const vm = createVM();
-    vm.freeze('app', app);
-    return vm;
+    return { vm, source, modules };
 }
 
-export default async () => {
-    if (!fs.existsSync('test/index.js')) {
-        return reject(new Error('no tests to run. [test/index.js] not found'));
-    }
-
+/**
+ * Compiles the app files and returns an instance of a V8 object.
+ *
+ * @return {Promise}
+ */
+async function compileApp() {
     const { name } = await readFile('package.json', 'utf-8')
         | JSON.parse;
-    const strings = await getLocaleStrings();
     const [source, modules] = await compileSources();
-    const vm = await runInSandbox(name, source, strings, modules)
-        | createTestVM;
-    const tests = await readFile('test/index.js', 'utf-8');
-    const result = vm.run(tests, 'test/index.js');
-    console.log(result);
-};
+    const strings = await getLocaleStrings();
+    return runInSandbox(name, source, strings, modules);
+}
+
+/**
+ * If possible, run the tests. Compile the tests to ES5 and the app sources to
+ * V8 objects, then run the tests in the VM linking the compiled object to it.
+ *
+ * @return {Promise}
+ */
+export default () => all([compileTests(), compileApp()])
+    .spread(async (test, app) => {
+        // Make `app` available to the tests
+        test.vm.freeze(app, 'app');
+        test.vm.options.require.mock = evaluateModules(test.vm, test.modules);
+    });
