bd212586bfc8a73a3c2cc6796ff62f08eb79ad44
Marcelo Camargo <marcelocamargo@linuxmail.org>
2018-05-07 15:59:44 -0300

    Testing no tests

--- test/salete/test.feat.js
+++ test/salete/test.feat.js
@@ -1,6 +1,11 @@
 import process from 'process';
 import { expect } from 'chai';
-import work, { keepCalm, createFile, createFolder } from './salete';
+import work, { keepCalm, createFile, createFolder, remove } from './salete';
+
+const test = {
+    runs: ['node', '../dist/cli.js', 'test'],
+    does: [keepCalm(20)]
+};
 
 // Inception! Mocha runs Salete that runs RungTests written by the user that are
 // written on top of Mocha and Chai. Too proud of it!
@@ -8,25 +13,29 @@ export default () => {
     before(~process.chdir('salete-hello-world'));
 
     it('should emit an error for Salete when she runs `rung test` without tests', () => {
-        return work({
-            runs: ['node', '../dist/cli.js', 'test'],
-            does: [keepCalm(20)] })
+        return work(test)
             .then(output => {
                 expect(output).to.contain('Error: no tests provided');
             });
     }).timeout(keepCalm(40));
 
-    /*
-    it('should create a static test file and run it successfully', () => {
+    it('should emit error when requiring an internal module in test file', () => {
         return createFolder('test')
-            .then(~createFile('test/index.js', [
-                'import { expect } from chai',
-                '',
-                'describe("Hello World!", () => {',
-                '  expect(1).to.be.equal(1)',
-                '})'
-            ].join('\n')));
-    });*/
+            .then(~createFile('test/index.js', 'import app from "../index";'))
+            .then(~work(test))
+            .then(output => {
+                expect(output).to.contain('Error: only external modules can be required in testsuite');
+            });
+    });
+
+    it('should tell when the test file exists, but there are no tests to run', () => {
+        return remove('test/index.js')
+            .then(~createFile('test/index.js', ''))
+            .then(~work(test))
+            .then(output => {
+                expect(output).to.contain('Info: no tests to run');
+            });
+    });
 
     after(~process.chdir('..'));
 };
